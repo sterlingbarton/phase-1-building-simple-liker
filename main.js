447d136a9c4ed0d583762c3c258c3d118621bf0d
Sterling <sterlingbarton81@gmail.com>
2023-04-02 11:47:58 -0600

    completed lab

--- main.js
+++ main.js
@@ -3,6 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let likeBtn = document.getElementsByClassName('like-glyph')
+  console.log(likeBtn)
+
+  for (let i = 0; i < likeBtn.length; i++) {
+    likeBtn[i].addEventListener('click', () => {
+      mimicServerCall().then((response) => {
+        console.log(response)
+        if (likeBtn[i].innerHTML === EMPTY_HEART) {
+          likeBtn[i].innerHTML = FULL_HEART;
+          likeBtn[i].classList.add('.activated-heart')
+        } else {
+          likeBtn[i].innerHTML = EMPTY_HEART;
+          likeBtn[i].classList.remove('.activated-heart')
+        }
+      
+    }).catch((error) => {
+      console.log(error)
+      let errorModal = document.querySelector('#modal')
+      errorModal.classList.remove('hidden')
+      let errorMessage = errorModal.querySelector('h2')
+      errorMessage.textContent = error
+      setTimeout(() => {
+      errorModal.classList.add('hidden')
+      }, 3000)
+    })
+  })
+  }
+   
 
 
 
